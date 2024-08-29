a19acccdfd360d522f133f4c606710a66b678968
kdjeetu <updesco@DESKTOP-08EPVTM.>
2024-08-29 12:24:10 +0000

    tiffin crud incomplete

---
+++ bottle/app/Http/Controllers/TiffinController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTiffinRequest;
+use App\Http\Requests\UpdateTiffinRequest;
+use App\Models\Tiffin;
+
+class TiffinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTiffinRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tiffin $tiffin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tiffin $tiffin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTiffinRequest $request, Tiffin $tiffin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tiffin $tiffin)
+    {
+        //
+    }
+}
